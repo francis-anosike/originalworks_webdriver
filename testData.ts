475be63f0544cf9eba9523164328faa50239f90b
francis-anosike <franosike@gmail.com>
2026-06-06 19:20:04 +0100

    utils/testData/helpers

---
+++ testData.ts
@@ -0,0 +1,30 @@
+export const signUpData = {
+    name: 'Francis',
+    email: 'francis${Date.now()}@gmail.com',
+    password: 'Test@1234'
+}
+
+export const signUpData2 = {
+    name: 'Francis',
+    email: 'francis@gmail.com',
+
+}
+
+export const loginData = {
+    email: 'francis@gmail.com',
+    password: 'fran1234'
+}
+
+
+export const addressData = {
+    firstName: 'Francis',
+    lastName: 'Nosike',
+    company: 'Nosike Corperation',
+    address1: '23 St pauls Street',
+    address2: 'Alderney Avenue',
+    country: 'New Zealand',
+    state: 'Milton Ford',
+    city: 'Bletchley',
+    zipcode: '442',
+    mobile: '0798765'
+}
